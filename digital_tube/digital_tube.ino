00b08bba085b9d07b2c906782fa849883a915f0d
Rui Xing <im.xingrui@gmail.com>
2022-11-30 10:44:26 +1100

    fix bug

--- digital_tube/digital_tube.ino
+++ digital_tube/digital_tube.ino
@@ -3,7 +3,6 @@ void setup() {
   for(int i=2;i<=9;i++){
     pinMode(i,OUTPUT);
   }
-
 }
 
 void loop() {
@@ -11,8 +10,4 @@ void loop() {
     digitalWrite(i,LOW);
   }
   delay(2000);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c24744c5db9df29ecd31326cc2f7e97bc376e5e1
